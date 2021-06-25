d133ab2c05d4063085946c57089a549eacde3e1e
OWO MAN <owoman@OWOs-Air.fios-router.home>
2021-06-25 15:45:07 -0400

    updated

--- contracts/contracts/TREELON.sol
+++ contracts/contracts/TREELON.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 
